107eee7a6c96f1fa1a9b7c6e780cf7c358fe6d3c
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-12 15:04:55 +0100

    ex 8

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>php</title>
+  </head>
+  <body>
+    <?php
+    $maVariable = 8;
+
+    if ($maVariable) {
+      echo 'c\'est ok !';
+    }
+    else {
+      echo 'c\'est pas bon !!!';
+    }
+     ?>
+  </body>
+</html>
